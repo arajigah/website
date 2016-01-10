680cea32997779b09c7fa5d75d143b456e7f74cb
ARajigah <andrew.rajigah@gmail.com>
2016-01-10 15:47:37 -0800

    more selenium!

--- src/main/resources/initial_data.sql
+++ src/main/resources/initial_data.sql
@@ -1,17 +1,9 @@
-INSERT INTO vendor (name) VALUES ('Vick''s Veggies');
-INSERT INTO vendor (name) VALUES ('Mel''s Meats');
-INSERT INTO vendor (name) VALUES ('Freddy''s Fruits');
-INSERT INTO foodcategory (category) VALUES ('Vegetable');
+INSERT INTO foodcategory (category) VALUES ('Vegetation');
 INSERT INTO foodcategory (category) VALUES ('Fruit');
 INSERT INTO foodcategory (category) VALUES ('Meat');
 INSERT INTO foodcategory (category) VALUES ('Fish');
+INSERT INTO foodcategory (category) VALUES ('Plankton');
+INSERT INTO foodcategory (category) VALUES ('Algae');
 INSERT INTO enclosurecondition (condition) VALUES ('Clean');
 INSERT INTO enclosurecondition (condition) VALUES ('Untidy');
 INSERT INTO enclosurecondition (condition) VALUES ('Dirty');
-
-INSERT INTO food (name, foodcategory_id, vendor_id) VALUES ('bananas', 2, 3);
-INSERT INTO food (name, foodcategory_id, vendor_id) VALUES ('salmon', 4, 2);
-INSERT INTO food (name, foodcategory_id, vendor_id) VALUES ('lettuce', 1, 1);
-INSERT INTO food (name, foodcategory_id, vendor_id) VALUES ('beef', 3, 2);
-INSERT INTO animal (infoLink, name, sciname, food_id) VALUES ('https://en.wikipedia.org/wiki/Grizzly_bear', 'Grizzly Bear', 'Ursus arctos horribilis', 2);
-INSERT INTO animal (infoLink, name, sciname, food_id) VALUES ('https://en.wikipedia.org/wiki/Chimpanzee', 'Chimpanzee', 'Pan', 1);
--- src/test/java/com/catalyst/zooKeepr/seleniumtest/EnclosureSeleniumTest.java
+++ src/test/java/com/catalyst/zooKeepr/seleniumtest/EnclosureSeleniumTest.java
@@ -1,9 +1,15 @@
 package com.catalyst.zooKeepr.seleniumtest;
 
+import static org.junit.Assert.assertTrue;
+
+import java.util.ArrayList;
+import java.util.List;
+
 import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
@@ -23,7 +29,22 @@ public class EnclosureSeleniumTest {
 	public void testAddNewEnclosure() {
 		driver.findElement(By.id("food")).click();
 		
-		new WebDriverWait(driver, 100).until(ExpectedConditions.presenceOfElementLocated(By.id("newFoodButton"))).click();
+		new WebDriverWait(driver, 400).until(ExpectedConditions.presenceOfElementLocated(By.id("newFoodButton"))).click();
+			driver.findElement(By.id("foodName")).sendKeys("Salmon");
+			driver.findElement(By.xpath("//*[@id='selectFoodCategory']")).click();
+			new WebDriverWait(driver, 400).until(ExpectedConditions.presenceOfElementLocated(By.xpath("//*[@id='selectFoodCategory']/option[5]"))).click();
+				driver.findElement(By.id("vendorName")).sendKeys("Sally's Salmon Deliveries");
+				driver.findElement(By.id("newFoodSave")).click();
+		
+		new WebDriverWait(driver, 400).until(ExpectedConditions.presenceOfElementLocated(
+				By.xpath("/html/body/div[2]/ui-view/div/table/tbody/tr"))).findElement(By.id("animal")).click();
+		new WebDriverWait(driver, 400).until(ExpectedConditions.presenceOfElementLocated(
+				By.id("newAnimalButton"))).findElement(By.id("newAnimalButton")).click();
+			new WebDriverWait(driver, 400).until(ExpectedConditions.presenceOfElementLocated(By.id("animalName"))).sendKeys("Grizzly Bear);
+		
+		
+		
+		
 		
 		new WebDriverWait(driver, 100).until(ExpectedConditions.presenceOfElementLocated(By.id("newEnclosureButton"))).click();
 		new WebDriverWait(driver, 300).until(ExpectedConditions.presenceOfElementLocated(By.id("enclosureName"))).sendKeys("Test Enclosure");
